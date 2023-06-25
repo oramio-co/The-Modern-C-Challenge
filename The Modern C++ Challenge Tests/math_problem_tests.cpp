0f300a69ad33bbfc32003b6c0787799cd9572b17
Oscar Ramirez <oscarjoramirez@yahoo.com>
2023-06-25 01:45:15 -0700

    add sum_multiples_alt test

--- The Modern C++ Challenge Tests/math_problem_tests.cpp
+++ The Modern C++ Challenge Tests/math_problem_tests.cpp
@@ -9,3 +9,13 @@ TEST(MathTest, sum_of_multiples_of_three_or_five) {
    }
    ASSERT_EQ(results, sums);
 }
+
+TEST(MathTest, sum_of_multiples_of_three_or_five_alt) {
+   std::vector<int> nums{ 3, 5, 10, 15, 30, 50, 100 };
+   std::vector<unsigned int> sums{ 3, 8, 33, 60, 225, 593, 2418 };
+   std::vector<unsigned int> results{};
+   for (auto num : nums) {
+      results.push_back(sum_multiples_alt(num));
+   }
+   ASSERT_EQ(results, sums);
+}
