5c78ccbe0c0d8fac209b061f15a1faa14d416f7f
Oscar Ramirez <oscarjoramirez@yahoo.com>
2023-06-25 02:45:52 -0700

    add fixutre for sum_multiple tests

--- The Modern C++ Challenge Tests/math_problem_tests.cpp
+++ The Modern C++ Challenge Tests/math_problem_tests.cpp
@@ -1,19 +1,23 @@
 #include "math_problems_test.h"
 
-TEST(MathTest, sum_of_multiples_of_three_or_five) {
-   std::vector<int> nums{ 3, 5, 10, 15, 30, 50, 100 };
-   std::vector<unsigned int> sums{ 3, 8, 33, 60, 225, 593, 2418 };
+class SumMultiplesTest : public testing::Test {
+protected:
+   void SetUp() override {
+      nums = { 3, 5, 10, 15, 30, 50, 100 };
+      sums = { 3, 8, 33, 60, 225, 593, 2418 };
+   }
+
+   std::vector<int>nums{};
+   std::vector<unsigned int> sums{};
    std::vector<unsigned int> results{};
+};
+TEST_F(SumMultiplesTest, sum_of_multiples_of_three_or_five) {
    for (auto num : nums) {
       results.push_back(sum_multiples(num));
    }
    ASSERT_EQ(results, sums);
 }
-
-TEST(MathTest, sum_of_multiples_of_three_or_five_alt) {
-   std::vector<int> nums{ 3, 5, 10, 15, 30, 50, 100 };
-   std::vector<unsigned int> sums{ 3, 8, 33, 60, 225, 593, 2418 };
-   std::vector<unsigned int> results{};
+TEST_F(SumMultiplesTest, sum_of_multiples_of_three_or_five_alt) {
    for (auto num : nums) {
       results.push_back(sum_multiples_alt(num));
    }
