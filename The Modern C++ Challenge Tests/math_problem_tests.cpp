7ff92c277bde3216bbe16d002856b3f42b78c444
Oscar Ramirez <oscarjoramirez@yahoo.com>
2023-06-25 01:41:24 -0700

    update sum_multiples test general

--- The Modern C++ Challenge Tests/math_problem_tests.cpp
+++ The Modern C++ Challenge Tests/math_problem_tests.cpp
@@ -1,7 +1,11 @@
 #include "math_problems_test.h"
 
 TEST(MathTest, sum_of_multiples_of_three_or_five) {
-   int num{ 15 };
-   int sum{ 60 };
-   ASSERT_EQ(sum_multiples(num), sum);
+   std::vector<int> nums{ 3, 5, 10, 15, 30, 50, 100 };
+   std::vector<unsigned int> sums{ 3, 8, 33, 60, 225, 593, 2418 };
+   std::vector<unsigned int> results{};
+   for (auto num : nums) {
+      results.push_back(sum_multiples(num));
+   }
+   ASSERT_EQ(results, sums);
 }
