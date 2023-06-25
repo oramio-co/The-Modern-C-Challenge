c05cace21ff6b92df0dd9045d66a0c40a3914092
Oscar Ramirez <oscarjoramirez@yahoo.com>
2023-06-25 01:43:01 -0700

    update sum_multiples general

--- The Modern C++ Challenge/src/math_problems.cpp
+++ The Modern C++ Challenge/src/math_problems.cpp
@@ -1,5 +1,15 @@
 #include "math_problems.h"
 
 unsigned int sum_multiples(int num) {
-   return 60;
+   unsigned int sum{ 0 };
+   const int MAX_NUM{ 135671 };
+   if (num > MAX_NUM) {
+      return sum;
+   }
+   for (int i = 1; i <= num; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+         sum += i;
+      }
+   }
+   return sum;
 }
