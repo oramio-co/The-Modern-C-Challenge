09a1f241e820d9f4751693918009f6c6f0a0179a
Oscar Ramirez <oscarjoramirez@yahoo.com>
2023-06-25 01:01:42 -0700

    update files for compile

---
+++ The Modern C++ Challenge Tests/include/math_problems_test.h
@@ -0,0 +1,7 @@
+#ifndef MATH_PROBLEMS_TEST_H
+#define MATH_PROBLEMS_TEST_H
+
+#include "gtest/gtest.h"
+#include "math_problems.h"
+
+#endif // !MATH_PROBLEMS_TEST_H
---
+++ The Modern C++ Challenge/include/math_problems.h
@@ -0,0 +1,6 @@
+#ifndef MATH_PROBLEMS_H
+#define MATH_PROBLEMS_H
+
+unsigned int sum_multiples(int num);
+
+#endif // !MATH_PROBLEMS_H
