115eaacb1536cc3ef51610561b83ab7ddec67e19
Oscar Ramirez <oscarjoramirez@yahoo.com>
2023-07-14 19:28:44 -0700

    add template for tests
    update tests
    
    update sum_multiples_alt test
    
    update sum_of_multiples test
    
    update greatest_common_denominator test
    
    update find_least_common_multiple test
    
    update find_largest_prime test
    
    update find_sexy_primes_pairs test
    
    update calculate_abundant_numbers test
    
    update calculate_amicable_numbers test
    
    update prime_factorization test
    
    update bin and gray_code tests
    
    update roman_numerals tests
    
    update first collatz_stopping_length test
    
    update second collatz_stopping_length

--- The Modern C++ Challenge Tests/include/math_problems_test.h
+++ The Modern C++ Challenge Tests/include/math_problems_test.h
@@ -4,4 +4,18 @@
 #include <gtest/gtest.h>
 #include <math_problems.h>
 
+template <typename T, typename U>
+void t_EXPECT_EQ(U(*func)(T), std::vector<T> results, std::vector<U> expected) {
+   for (std::size_t i = 0; i < results.size(); ++i) {
+      EXPECT_EQ(func(results[i]), expected[i]);
+   }
+}
+
+template <typename T, typename U>
+void t_EXPECT_EQ(U(*func)(T, T), std::vector<T> first, std::vector<T> second, std::vector<U> expected) {
+   for (std::size_t i = 0; i < first.size(); ++i) {
+      EXPECT_EQ(func(first[i], second[i]), expected[i]);
+   }
+}
+
 #endif // !MATH_PROBLEMS_TEST_H
